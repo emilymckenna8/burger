7d1d47b2ca410c39311ccc2cfa4bff940222a379
Emily McKenna <emilymckenna08@gmail.com>
2018-08-02 13:04:17 -0400

    initial directory setup

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('The Green Ghost');
+INSERT INTO burgers (burger_name) VALUES ('Shack Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Breakfast Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mexi-Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bison Burger', true);
